5d62eb3d9f8abd8db42101751da2ddc59e2784a9
nhmunna <munna1720.nhm@gmail.com>
2021-09-02 19:06:52 +0600

    finished

--- book.js
+++ book.js
@@ -13,7 +13,7 @@ const searchBook = () => {
     }
     else {
         // load data
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(response => response.json())
             .then(data => displaySearchResult(data.docs))
